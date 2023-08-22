a899b697c695c8804161796158d31df9f31db7a2
Michael Harrison <mah5044@gmail.com>
2023-08-21 21:43:33 -0700

    Initial Commit

---
+++ Math4Kids/Math4KidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math4KidsApp.swift
+//  Math4Kids
+//
+//  Created by Michael Harrison on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Math4KidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
